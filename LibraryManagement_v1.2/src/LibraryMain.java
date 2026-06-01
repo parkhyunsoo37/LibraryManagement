84f0d72dd55bccca08ba4227bb812129a47b0257
parkhyunsoo37 <hyunsustar21@gmail.com>
2026-06-01 22:32:29 +0900

    Github Issue #1 해결 완료
    책 삭제 db에서 해당 책이 삭제 안됨
    
    https://github.com/parkhyunsoo37/LibraryManagement/issues/1

--- LibraryManagement_v1.2/src/LibraryMain.java
+++ LibraryManagement_v1.2/src/LibraryMain.java
@@ -152,6 +152,8 @@ public class LibraryMain {
     /**
      * 도서 정보의 수정 및 삭제를 처리하는 UI입니다.
      * <p>ID를 통해 도서를 조회하고, 선택에 따라 제목/저자 수정 또는 삭제를 수행합니다.</p>
+     *<p>수정과 삭제의 효율화를 위해 같은 기능으로 개발(26.05.20; 박현수)</p>
+     * @see <a href="https://github.com/parkhyunsoo37/LibraryManagement/issues/1">Issue #21: 한 책 삭제 시 DB에서 해당 책이 ㅣ삭제 안됨</a>
      *
      * @see LibraryManager#deleteBook(int)
      */
@@ -201,6 +203,7 @@ public class LibraryMain {
             }
             case 3 -> {
                 manager.deleteBook(id);
+                //삭제 쿼리를 작성
                 System.out.println("[결과] 삭제되었습니다.");
 
 
